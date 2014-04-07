25e9691e619d85e60cb71da4ac80e081725a8c8d
BenJohnson <BenJohnson@bkj-ycas.wireless.yale.internal>
2014-04-07 15:09:22 -0400

    V2

--- rrest.R
+++ rrest.R
@@ -5,36 +5,31 @@ require(httpuv)
 require(RJSONIO)
 
 restServer <- function(fun_env) {
-	function(env){
-		
-		method <- env[['REQUEST_METHOD']]
-	
-		resp   <- '{"null_response" : undefined}'	
-		if(method == 'POST') {
-			
-			postfields <- rawToChar(env[['rook.input']]$read())
-			postfields <- fromJSON(postfields)
-			if(length(postfields) == 2){
-				fun    <- exec_env[[postfields[['fun']]]]
-
-				if(!is.null(fun)){
-					params <- postfields[['params']]
-					resp   <- toJSON(fun(params))
-				} else {
-					print('unsupported function!')
-				}
-			}
-			
-		} else {
-			print('unsupported request!')
-		}
-		
-		list(
-	    	status = 200L,
-	    	headers = list('Content-Type' = 'JSON'),
-	    	body = resp
-		)	
+  function(env){
+    method <- env[['REQUEST_METHOD']]
+    resp   <- '{"null_response" : undefined}'	
+    if(method == 'POST') {	
+	  postfields <- rawToChar(env[['rook.input']]$read())
+	  postfields <- fromJSON(postfields)
+	  if(length(postfields) == 2){
+	    fun    <- exec_env[[postfields[['fun']]]]
+	    if(!is.null(fun)){
+	      params <- postfields[['params']]
+	      resp   <- toJSON(fun(params))
+	    } else {
+	      print('unsupported function!')
+	    }
+	  }
+	} else {
+	  print('unsupported request!')
 	}
+		
+	list(
+	  status = 200L,
+	  headers = list('Content-Type' = 'JSON'),
+	  body = resp
+	)	
+  }
 }
 
 
@@ -43,13 +38,13 @@ restServer <- function(fun_env) {
 
 # Define Sample Functions...
 randomUniform <- function(p){
-	p <- lapply(p, as.numeric)
-	runif(p$n)
+  p <- lapply(p, as.numeric)
+  runif(p$n)
 }
 
 randomNormal <- function(p){
-	p <- lapply(p, as.numeric)
-	rnorm(p$n)
+  p <- lapply(p, as.numeric)
+  rnorm(p$n)
 }
 
 fun_env <- new.env()
