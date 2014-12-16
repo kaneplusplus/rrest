c4af573d1f0edb63d3b61acf65d599f3f40c68fc
bkj <ben@gophronesis.com>
2014-12-15 21:27:21 -0500

    Changed service to take name of function from URL route rather than as parameter. Allow for multiple methods called on same endpoint. Changed format of POSTs to be more canonical. Made call a function of `fun_env`.

--- service.r
+++ service.r
@@ -1,6 +1,7 @@
 library(parallel)
 library(RJSONIO)
 
+source('responses.R')
 rrest_socket_connection = function(host="localhost", port=9090, fail=TRUE) {
   conn = new.env()
   assign("host", host, conn)
@@ -25,8 +26,25 @@ next_request.rrest_socket_connection = function(conn) {
   conn$sock = make.socket(host=conn$host, port=conn$port, fail=conn$fail,
                           server=TRUE)
   str = read.socket(conn$sock, maxlen=10000L)
-  req = unlist(strsplit(str, "\r\n"))
-  req = req[length(req)]
+  
+  # I wonder if we should separate parsing from reading
+  # because I'm guessing HTTP requests are going to look
+  # the same no matter how we actuall receive them (sockets, 
+  # httpuv, whatever)
+  meth     = unlist(strsplit(str, ' /'))[[1]]
+  endpoint = unlist(regmatches(str, gregexpr('/[^ ]*', str)))[1]
+  endpoint = gsub('^/', '', endpoint)
+  body     = unlist(strsplit(str, "\r\n"))
+  body     = body[length(body)]
+  if(jsonlite::validate(body)) {
+	  body = fromJSON(body)
+  } else {
+	  body = NULL
+  }
+
+  req = list(fun = endpoint, body = body)
+  class(req) = c('character', meth)
+  req
 }
 
 send_response.rrest_socket_connection = function(conn, x) {
@@ -38,12 +56,32 @@ close.rrest_socket_connection = function(conn) {
   conn$sock = NULL
 }
 
-start_service = function(conn, call, callback=NULL, parallel=1, 
+call = function(req, fun_env) {
+	toJSON(
+		tryCatch({
+			fun <- fun_env[[req[['fun']]]]
+			print(fun)
+			if(!is.null(fun)){
+				body <- req[['body']]
+				if(!is.null(body)) {
+					class(body) <- c(class(body), class(req))
+				}
+				success(toJSON(fun(body)))
+			} else { 
+				method_not_found()
+			}
+		}, error = function(e) {
+			throw_error()
+		})
+	)
+}
+
+start_service = function(conn, fun_env, callback=NULL, parallel=1, 
                          max_num_requests=Inf) {
   if (parallel == 1) {
     i=1
     while(i <= max_num_requests) {
-      call_ret = call(next_request(conn))
+      call_ret = call(next_request(conn), fun_env)
       if (!is.null(call_ret))
         send_response(conn, call_ret)
       if (!is.null(callback))
