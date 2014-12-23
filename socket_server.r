004e58c979552a10e40717616cc20cf4b4272085
Michael Kane <kaneplusplus@gmail.com>
2014-12-23 17:21:08 -0500

    Initial version of the socket server and R bindings.

--- socket_server.r
+++ socket_server.r
@@ -1,7 +1,10 @@
-while (1) {
-  sock = make.socket(host = "localhost", port="9090", fail = TRUE, 
-                     server = TRUE)
-  str = read.socket(sock)
-  print(str)
-  close.socket(sock)
+library(attributes)
+
+if (!exists("params_updated")) {
+  Sys.setenv("PKG_LIBS"=paste("-lboost_system-mt", Sys.getenv("PKG_LIBS")))
+  Sys.setenv("PKG_LIBS"=paste("-L/usr/local/lib/",Sys.getenv("PKG_LIBS")))
+  params_updated=TRUE
 }
+attributes::sourceCpp("boost_socket.cpp", verbose=TRUE)
+
+
