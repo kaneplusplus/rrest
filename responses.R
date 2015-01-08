0218ee3354d5be0a9b78ded3b3c6eda76fa92f20
bkj <ben@gophronesis.com>
2015-01-08 14:44:21 -0500

    fixed braces

--- responses.R
+++ responses.R
@@ -2,25 +2,22 @@
 success <- function(resp) {
   paste("HTTP/1.1 200 OK",
         "Content-Type: text/json",
-        paste("Content-Length:", nchar(resp, type="bytes")+4),
-        "",
-        "{", resp, "}\n", sep="\n") 
+        paste("Content-Length:", nchar(resp, type="bytes")),
+        "", resp, sep="\n") 
 }
 
 method_not_found <- function() {
   resp = "Method not allowed"
   paste("HTTP/1.1 405 Method Not Allowed",
         "Content-Type: text",
-        paste("Content-Length:", nchar(resp, type="bytes")+4),
-        "",
-        "{", resp, "}\n", sep="\n")
+        paste("Content-Length:", nchar(resp, type="bytes")),
+        "", resp, sep="\n")
 }
 
 throw_error <- function() {
   resp = "Internal server error occured"
   paste("HTTP/1.1 500 Internal Server Error Occured",
         "Content-Type: text",
-        paste("Content-Length:", nchar(resp, type="bytes")+4),
-        "",
-        "{", resp, "}\n", sep="\n")
+        paste("Content-Length:", nchar(resp, type="bytes")),
+        "", resp, sep="\n")
 }
